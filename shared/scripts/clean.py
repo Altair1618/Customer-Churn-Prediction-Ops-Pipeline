3123b754a7bd4d18faec8cb55050c3c3bfccd967
frankiehuangg <13521092@std.stei.itb.ac.id>
2025-01-10 16:55:20 +0700

    feat: implement preprocessing code

--- shared/scripts/clean.py
+++ shared/scripts/clean.py
@@ -1,10 +1,22 @@
 from pyspark.sql import SparkSession
-from pyspark.ml.feature import StringIndexer
+from pyspark.sql.functions import col, when, udf
+from pyspark.sql.types import IntegerType, FloatType
 
 
 input_path = "/shared/data/customer_churn.csv"
 output_path = "/shared/processed/customer_churn_cleaned.parquet"
 
+def convert_to_binary(datum: str) -> int:
+    if datum == 'No':
+        return 0  
+    return 1
+
+def convert_to_service_code(datum: str) -> int:
+    if datum == 'No':
+        return 0
+    if datum == 'Yes':
+        return 1
+    return 2
 
 def clean(input_path, output_path):
     spark = SparkSession \
@@ -17,18 +29,43 @@ def clean(input_path, output_path):
     data = data.drop("customerID")
     data = data.dropna().dropDuplicates()
 
-    categorical_columns = [col for col, dtype in data.dtypes if dtype == "string"]
-    for col in categorical_columns:
-        indexer = StringIndexer(inputCol=col, outputCol=f"{col}_temp")
-        data = indexer.fit(data).transform(data)
-        data = data.drop(col)
+    data = data.withColumn('gender', when(col('gender') == 'Female', 0).otherwise(1))
 
-    for col in data.columns:
-        if col.endswith("_temp"):
-            data = data.withColumnRenamed(col, col[:-5])
+    data = data.withColumn('MultipleLines', when(col('MultipleLines') == 'No', 0)
+                                        .when(col('MultipleLines') == 'Yes', 1)
+                                        .otherwise(2))
+
+    data = data.withColumn('InternetService', when(col('InternetService') == 'No', 0)
+                                         .when(col('InternetService') == 'DSL', 1)
+                                         .otherwise(2))
+
+    data = data.withColumn('PaymentMethod', when(col('PaymentMethod') == 'Bank transfer (automatic)', 0)
+                                        .when(col('PaymentMethod') == 'Credit card (automatic)', 1)
+                                        .when(col('PaymentMethod') == 'Electronic check', 2)
+                                        .otherwise(3))
+
+    data = data.withColumn('Contract', when(col('Contract') == 'Month-to-month', 0)
+                                    .when(col('Contract') == 'One year', 1)
+                                    .otherwise(2))
+
+    data = data.withColumn('MonthlyCharges', col('MonthlyCharges').cast(FloatType()))
+    data = data.withColumn('TotalCharges', col('TotalCharges').cast(FloatType()))
+
+    binary_columns = ['Partner', 'Dependents', 'PhoneService', 'PaperlessBilling', 'Churn']
+
+    for column in binary_columns:
+        data = data.withColumn(column, binary_udf(col(column)))
+
+    trinary_columns = ['OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies']
+
+    for column in trinary_columns:
+        data = data.withColumn(column, service_code_udf(col(column)))
 
     data.write.parquet(output_path, mode="overwrite")
 
 
 if __name__ == "__main__":
+    binary_udf = udf(convert_to_binary, IntegerType())
+    service_code_udf = udf(convert_to_service_code, IntegerType())
+
     clean(input_path, output_path)
